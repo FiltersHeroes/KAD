27032106360eda0f58bbd38f8be9bce6fc8c1380
hawkeye116477 <hawkeye116477@gmail.com>
2019-08-19 18:25:00 +0200

    Aktualizacja skryptu [ci skip]

--- scripts/VICHS.sh
+++ scripts/VICHS.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # VICHS - Version Include Checksum Hosts Sort
-# v2.8.1
+# v2.8.2
 
 # MIT License
 
@@ -511,8 +511,7 @@ for i in "$@"; do
     # Sprawdzanie czy aktualizacja naprawdę jest konieczna
     if [ "$old_md5" != "$new_md5" ] || [ "$FORCED" ]; then
         # Aktualizacja daty i godziny w polu „Last modified"
-        export LC_ALL=en_US.UTF-8
-        modified=$(date +"$(grep -oP -m 1 '@dateFormat \K.*' "$CONFIG")")
+        modified=$(LC_TIME=en_US.UTF-8; date +"$(grep -oP -m 1 '@dateFormat \K.*' "$CONFIG")")
         sed -i "s|@modified|$modified|g" "$i"
 
         # Aktualizacja wersji
@@ -534,8 +533,7 @@ for i in "$@"; do
         sed -i "s|@version|$version|g" "$i"
 
         # Aktualizacja pola „aktualizacja"
-        export LC_ALL=pl_PL.UTF-8
-        aktualizacja=$(date +"$(grep -oP -m 1 '@dateFormat \K.*' "$CONFIG")")
+        aktualizacja=$(LC_TIME=pl_PL.UTF-8; date +"$(grep -oP -m 1 '@dateFormat \K.*' "$CONFIG")")
         sed -i "s|@aktualizacja|$aktualizacja|g" "$i"
 
         # Aktualizacja sumy kontrolnej
