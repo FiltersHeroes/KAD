3cc3f672b8a694de7073db23d3d40d89ac32a4bc
hawkeye116477 <hawkeye116477@gmail.com>
2020-04-13 22:01:37 +0200

    Nowa lista

--- scripts/VICHS_auto.sh
+++ scripts/VICHS_auto.sh
@@ -4,26 +4,10 @@
 sciezka=$(dirname "$0")
 
 cd $sciezka/..
-
+$sciezka/VICHS.sh ./KAD.txt ./KADfake.txt
+cd ..
 if [ "$CI" = "true" ] ; then
-    ost_plik=$(git diff-tree --no-commit-id --name-only -r master)
-else
-    ost_plik=$(git diff -z --name-only | xargs -0)
+    git clone git@github.com:PolishFiltersTeam/KADhosts.git
 fi
-
-
-if [[ "$ost_plik" == *"sections"* ]]; then
-    if [[ "$lista" != *" KAD.txt"* ]] ;then
-        lista+=" "KAD.txt
-    fi
-fi
-
-if [ "$lista" ] ; then
-    $sciezka/VICHS.sh $lista
-    cd ..
-    if [ "$CI" = "true" ] ; then
-        git clone git@github.com:PolishFiltersTeam/KADhosts.git
-    fi
-    cd ./KADhosts
-    ./scripts/VICHS.sh ./KADhosts.txt ./KADhosts_without_controversies.txt ./KADhole.txt ./KADfakeHosts.txt
-fi
+cd ./KADhosts
+./scripts/VICHS.sh ./KADhosts.txt ./KADhosts_without_controversies.txt ./KADhole.txt ./KADfakeHosts.txt
