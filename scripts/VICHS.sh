44220a4c2070df61589c9d26a130795fbb3aa556
hawkeye116477 <hawkeye116477@gmail.com>
2020-05-03 16:50:54 +0200

    Aktualizacja skryptu
    [ci skip]

--- scripts/VICHS.sh
+++ scripts/VICHS.sh
@@ -572,11 +572,11 @@ for i in "$@"; do
         # Commitowanie zmienionych plików
         if [ "$CI" = "true" ] ; then
             commit_desc=$(grep -oP -m 1 '@commitDesc \K.*' "$CONFIG")
-            git commit -m "$(eval_gettext "Update \$filter to version \$version")" -m "${commit_desc} [ci skip]"
+            git commit -m "$(eval_gettext "Update \$filter to version \$version")" -m "${commit_desc}" -m "[ci skip]"
         else
             printf "%s" "$(eval_gettext "Enter extended commit description to \$filter list, e.g 'Fix #1, fix #2' (without quotation marks; if you do not want an extended description, you can simply enter nothing): ")"
             read -r extended_desc
-            git commit -m "$(eval_gettext "Update \$filter to version \$version")" -m "${extended_desc} [ci skip]"
+            git commit -m "$(eval_gettext "Update \$filter to version \$version")" -m "${extended_desc}" -m "[ci skip]"
         fi
     else
         printf "%s\n" "$(eval_gettext "Nothing new has been added to \$filter list. If you still want to update it, then set the variable FORCED and run script again.")"
