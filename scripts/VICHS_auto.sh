4708d3727350d7e4361115465fb7b06be1355b94
hawkeye116477 <hawkeye116477@gmail.com>
2021-03-16 19:00:06 +0100

    Poprawka

--- scripts/VICHS_auto.sh
+++ scripts/VICHS_auto.sh
@@ -10,6 +10,13 @@ cd $SCRIPT_PATH/..
 #$SCRIPT_PATH/ADD_NOVELTIES_FROM_CERT.sh
 #$SCRIPT_PATH/ADD_NOVELTIES_FROM_LWS.sh
 #$SCRIPT_PATH/ADD_NOVELTIES_FROM_CSMI.sh
+
+rm -rf ./sections/podejrzane_inne_oszustwa.txt
+mv ./sections/LWS/podejrzane_inne_oszustwa.txt ./sections/
+
+rm -rf ./sections/przekrety.txt
+mv ./sections/CERT/przekrety.txt ./sections/
+
 ost_plik=$(git diff --name-only --pretty=format: | sort | uniq)
 function search() {
     echo "$ost_plik" | grep "$1"
@@ -25,7 +32,7 @@ fi
 NO_PUSH="true" $SCRIPT_PATH/VICHS.sh ./KAD.txt
 cd ..
 if [ "$CI" = "true" ] ; then
-    git clone git@github.com:PolishFiltersTeam/KADhosts.git
+    git clone https://github.com/PolishFiltersTeam/KADhosts.git
 fi
 cd ./KADhosts
 NO_PUSH="true" ./scripts/VICHS.sh ./KADhosts.txt /KADhole.txt ./KADomains.txt
