f3f3a260d07f1b1d65deb325904a2d3e1287ca35
krystian3w <35370833+krystian3w@users.noreply.github.com>
2025-01-01 01:25:41 +0100

    Update Offline.py

--- scripts/Offline.py
+++ scripts/Offline.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 """MIT License
 
-Copyright (c) 2021 Filters Heroes
+Copyright (c) 2025 Filters Heroes
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
